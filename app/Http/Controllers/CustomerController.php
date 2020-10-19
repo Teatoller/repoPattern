844c492ac9961e561360833a37154cdf6c0e4cf5
Teatoller <steven.otieno1@gmail.com>
2020-10-19 21:12:48 +0300

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $customer = Customer::all();
+        $customer = Customer::orderBy('name')
+        ->where('active', 1)
+        ->with('user')
+        ->get()
+        ->map(function($customer){
+            return [
+                'customer_id'=> $customer->id,
+                'name' => $customer->name,
+                'created_by'=> $customer->user->email,
+                'last_updated'=> $customer->updated_at->diffForHumans(),
+            ];
+        });
+
+        return $customer;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
